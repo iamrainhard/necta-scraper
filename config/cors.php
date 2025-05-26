1b6272769ccc63bdd5796be1c91d7a800a6d735d
Rainhard Ngambiye <rainhardtz@hotmail.com>
2025-05-27 00:02:07 +0300

    ipdated cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://jcas.janesa.ac.tz'],
+    'allowed_origins' => ['https://jcas.janesa.ac.tz, https://apiv2.janesa.ac.tz'],
 
     'allowed_origins_patterns' => [],
 
